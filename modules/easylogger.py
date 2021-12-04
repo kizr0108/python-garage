87bf5bd66210f18d5241017d135664034fe8b3d8
kizr0108 <65641277+kizr0108@users.noreply.github.com>
2021-12-04 09:32:29 +0900

    Update easylogger.py

--- modules/easylogger.py
+++ modules/easylogger.py
@@ -72,7 +72,7 @@ class EasyLogger:
                     frameinfo = inspect.stack()
                     for i in range(2 if len(frameinfo)> 2 else len(frameinfo)):
                         stack = frameinfo[-i-1]
-                        print(stack.filename)
+                        self.info(stack.filename)
                         echo(stack.filename, stack.lineno, stack.function)
                     return func(*args, **kwargs)
                 except Exception as e:
